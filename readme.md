bea93f3c03d88e583c756ece12fca08b8f0df659
Germán Alonso Pérez <german.alonso.sspa@juntadeandalucia.es>
2023-01-24 14:24:43 +0100

    feat: added new method on trails to create user and update it relating to LDAP user
    feat: readme.md modified

--- readme.md
+++ readme.md
@@ -1,17 +1,36 @@
 <!-- readme.md -->
 
+<p >Integrate LDAP using LDAP Record into your Laravel application for SAS.</p>
 
-<p align="center">Integrate LDAP using LDAP Record into your Laravel application for SAS.</p>
-
-<p align="center">
-    <a href="https://laravel.com"><img src="https://img.shields.io/badge/Built_for-Laravel-green.svg?style=flat-square"></a>
+<p >
+    <a href="https://laravel.com"><img alt="" src="https://img.shields.io/badge/Built_for-Laravel-green.svg?style=flat-square"></a>
     <a href="https://github.com/DirectoryTree/LdapRecord-Laravel/actions"></a>
-<img src="https://img.shields.io/packagist/l/directorytree/ldaprecord-laravel.svg?style=flat-square"></a>
+<img alt="" src="https://img.shields.io/packagist/l/directorytree/ldaprecord-laravel.svg?style=flat-square">
 </p>
 
 ---
 
 🔑  **Authenticate LDAP users into your application**
 
-Allow LDAP users to log into your application using [LDAPRecord for Laravel](https://ldaprecord.com/docs/laravel/v1/auth/configuration/#rules).
+Allow LDAP users to log into your application using [LDAPRecord for Laravel](https://ldaprecord.com/docs/laravel/v2).
 
+<b>IMPORTANT: </b> After installation of this package, execute <br>
+``` php artisan vendor:publish --provider="LdapRecord\Laravel\LdapServiceProvider" ```
+<br>
+to publish LDAPRecord config files.
+Then in your .env, add the required params. Example:
+``` 
+LDAP_HOST=""
+LDAP_BASE_DN=""
+LDAP_USERNAME=""
+LDAP_PASSWORD=""
+LDAP_LOGGING=true
+LDAP_CONNECTION=default
+LDAP_PORT=389
+LDAP_TIMEOUT=5
+LDAP_SSL=false
+LDAP_TLS=false
+ ```
+
+
+<p >Germán Alonso Pérez </p>
--- src/SASLdapAuthenticate.php
+++ src/SASLdapAuthenticate.php
@@ -2,21 +2,61 @@
 
 namespace SASLdap;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Str;
 use LdapRecord\Container;
+use LdapRecord\Models\Model;
 use SASLdap\Model\ActiveDirectoryUser;
 
 trait SASLdapAuthenticate{
 
-    public function activeDirectoryUserAttempt($email, $password): ?\LdapRecord\Models\Model
+
+    /**
+     * Checks against active directory the credentials given
+     * @param $name string Common name (cn) of this user in the LDAP server
+     * @param $password string Password of this account
+     * @return Model|null null if user does not exists
+     */
+    public function activeDirectoryUserAttempt(string $name, string $password): ?Model
     {
         $connection = Container::getDefaultConnection();
         try{
-            $user = ActiveDirectoryUser::findByOrFail('cn', $email);
+            $user = ActiveDirectoryUser::findByOrFail('cn', $name);
             return $connection->auth()->attempt($user->getDn(), $password) ? $user : null;
         }
         catch (Exception) {
             return null;
         }
+    }
+
+    /**
+     * If user in the LDAP server exists, it logs user in the system using the default App\User model. If the user does not exists,
+     * @param $name string Common name (cn) of this user in the LDAP server
+     * @param $password string Password of this account
+     * @param bool $remember default false
+     * @return User|null Authenticated user or nul if this does not exists
+     */
+    public function authenticate(string $name, string $password, bool $remember = false): ?\App\Models\User
+    {
+        $name = strtolower($name);
+        $ad_user = $this->activeDirectoryUserAttempt($name, $password);
+        if($ad_user){
+            //Persistimos este usuario en el sistema
+            $user = User::where('dmsas', $name)->first();
+            if($user == null){
+                //Password set as random because it cannot be null
+                $user = User::firstOrCreate(['name' => $ad_user->getName() ?? $name, 'dmsas' => $name, 'password' => Crypt::encryptString(Str::random())]);
+            }
+            $user->last_login_at = Carbon::now()->toDateTimeString();
+            $user->save();
+            Auth::login($user, $remember);
+            return $user;
+        }
+        return null;
     }
 }
